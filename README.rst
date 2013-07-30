38312ea5886fb87859f704ed370d9cd874595b8b
Simon de Haan <simon@praekeltfoundation.org>
2013-07-30 18:02:10 +0200

    initial layout

---
+++ README.rst
@@ -0,0 +1,5 @@
+Go-FormHub
+==========
+
+A Vumi Go Javascript application that reads FormHub JSON form descriptions
+and surfaces them as best as possible via USSD.
---
+++ test/test-go-formhub.js
@@ -0,0 +1,19 @@
+var fs = require("fs");
+var assert = require("assert");
+var vumigo = require("vumigo_v01");
+// CHANGE THIS to your-app-name
+var app = require("../lib/go-formhub");
+
+// This just checks that you hooked you InteractionMachine
+// up to the api correctly and called im.attach();
+describe("test_api", function() {
+    it("should exist", function() {
+        assert.ok(app.api);
+    });
+    it("should have an on_inbound_message method", function() {
+        assert.ok(app.api.on_inbound_message);
+    });
+    it("should have an on_inbound_event method", function() {
+        assert.ok(app.api.on_inbound_event);
+    });
+});
